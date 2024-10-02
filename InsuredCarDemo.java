5f0979b9b4478770014bd20001531ac44ecb095d
Veejay <veejaycuizon24@gmail.com>
2024-10-02 17:36:23 +0800

    Using records, Anonymous Inner Classes, and Lambda Expressions

---
+++ InsuredCarDemo.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+public class InsuredCarDemo {
+    public static void main(String[] args) {
+        InsuredCar myCar = new InsuredCar();
+        JOptionPane.showMessageDialog(null, myCar.toString());
+    }
+}
