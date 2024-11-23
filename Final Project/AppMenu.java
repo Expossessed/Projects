a77e142ef8bc366daac39c56badc1284ee88b196
Veejay <veejaycuizon24@gmail.com>
2024-11-23 21:12:27 +0800

    kapoy na

--- Final Project/AppMenu.java
+++ Final Project/AppMenu.java
@@ -17,24 +17,43 @@ public class AppMenu {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(true);
-        frame.setSize(1000,1000);
+        frame.setSize(1015,1000);
         frame.setLayout(null);
 
+        JPanel header = new JPanel();
+        header.setBounds(0,0,1000,60);
+        header.setLayout(null);
+        header.setOpaque(false);
+        ImageIcon headerbg = new ImageIcon("Header.jpg");
+        Image resizedhead = headerbg.getImage().getScaledInstance(1000, 60, Image.SCALE_SMOOTH);
+        ImageIcon resizedheader = new ImageIcon(resizedhead);
+        JLabel headerlabel = new JLabel();
+        
+        headerlabel.setBounds(0,0,1000,60);
+        headerlabel.setIcon(resizedheader);
+        headerlabel.setVerticalAlignment(JLabel.TOP);
+        headerlabel.setHorizontalAlignment(JLabel.CENTER);
+        header.add(headerlabel);
+
+        header.add(headerlabel, BorderLayout.CENTER);
+        
         JPanel menu = new JPanel();
-        menu.setBounds(0,0,1000,680);
-        menu.setLayout(new BorderLayout());
-
+        menu.setBounds(0,60,1000,700);
+        menu.setLayout(null);
         ImageIcon menubg = new ImageIcon("Menubg.jpg");
-        Image resizedbg = menubg.getImage().getScaledInstance(1000, 680, Image.SCALE_SMOOTH);
+        Image resizedbg = menubg.getImage().getScaledInstance(1000, 700, Image.SCALE_SMOOTH);
         ImageIcon resizedmenu = new ImageIcon(resizedbg);
-        JLabel label = new JLabel();
+        JLabel menulabel = new JLabel();
 
-        label.setIcon(resizedmenu);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setHorizontalAlignment(JLabel.CENTER);
+        menulabel.setBounds(0,0,1000,700);
+        menulabel.setIcon(resizedmenu);
+        menulabel.setVerticalAlignment(JLabel.TOP);
+        menulabel.setHorizontalAlignment(JLabel.CENTER);
+        menu.add(menulabel);
 
-        menu.add(label, BorderLayout.CENTER);
+        menu.add(menulabel, BorderLayout.CENTER);
         frame.add(menu);
+        frame.add(header);
 
         JButton button = new JButton();
         button.setBounds(400, 700 , 200, 50);
